fa90108e942ffc8045fa3742335e00681af7e487
Mario Tobar <104882791+mario-th@users.noreply.github.com>
2023-03-03 23:47:55 -0600

    Add files via upload

---
+++ EntradaSalida/EntradaSalida/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class EntradaSalida
+    {
+        public static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
