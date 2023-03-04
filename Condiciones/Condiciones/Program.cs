0b983a46d085f965624f8b13553336754e87bb4a
Mario Tobar <104882791+mario-th@users.noreply.github.com>
2023-03-03 23:52:29 -0600

    Add files via upload

---
+++ Condiciones/Condiciones/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sentencias
+{
+    class SentenciasIf
+    {
+        public static void Main(string[] args)
+        {
+            int numeroEntero = int.Parse(Console.ReadLine());
+
+            if (numeroEntero == 7)
+                Console.WriteLine("numeroEntero es 7.");
+            else if (numeroEntero < 0)
+                Console.WriteLine("numeroEntero es negativo");
+            else
+                Console.WriteLine("numeroEntero no es ninguno de los casos programados");          
+        }
+    }
+}
