1981c2bcb675511f7871c2a6a57789bf3de344c9
Mario Tobar <104882791+mario-th@users.noreply.github.com>
2023-03-03 23:55:26 -0600

    Add files via upload

---
+++ LeerTeclado/LeerTeclado/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LeerTeclado
+{
+    /// <summary>
+    /// Leer datos desde teclado
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de dos numeros");
+            Console.Write("Ingrese numero 1: ");
+            // Los datos se leen como cadena
+            string dato = Console.ReadLine();
+            // Convertir a numero int.Parse
+            int n1 = int.Parse(dato);
+
+            Console.Write("Ingrese numero 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1+ n2;
+            Console.WriteLine("El resultado es: {0}", suma);
+        }
+    }
+}
